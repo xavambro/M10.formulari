d61bb30fde01f5e2e186f50e2fe36c75b1340b71
xavambro <xavambro@gmail.com>
2021-03-09 17:30:27 +0100

    Arregladas validaciones y reseteo de valores al hacer submit

--- src/components/Formulari.vue
+++ src/components/Formulari.vue
@@ -3,13 +3,13 @@
   <form @submit.prevent="sendForm">
     <h1>Formulari</h1>
     <p v-if='errors' class="alert alert-danger">No es pot validar el formulari. Ompla els camps</p>
-    <p v-if='valid' class="alert alert-success">Formulari validat correctament.</p>
-    <FormInput type="name" label="Nom:" input="inputName"></FormInput>
-    <FormInput type="mobile" label="Mòbil:" input="inputMobile"></FormInput>
-    <FormInput type="postal" label="Codi Postal:" input="inputPostal"></FormInput>
-    <FormInput type="email" label="Email:" input="inputEmail"></FormInput>
-    <FormInput type="pass" label="Password:" input="inputPass" @pass='getPass($event)'></FormInput>
-    <FormInput type="pass1" label="Repeteix password:" input="inputPass1" :pass='pass'></FormInput>
+    <p v-if='valid' class="alert alert-success">Formulari validat i enviat correctament.</p>
+    <FormInput type="name" label="Nom:" input="inputName" :reset='valid'></FormInput>
+    <FormInput type="mobile" label="Mòbil:" input="inputMobile" :reset='valid'></FormInput>
+    <FormInput type="postal" label="Codi Postal:" input="inputPostal" :reset='valid'></FormInput>
+    <FormInput type="email" label="Email:" input="inputEmail" :reset='valid'></FormInput>
+    <FormInput type="pass" label="Password:" input="inputPass" :reset='valid' @pass='getPass($event)'></FormInput>
+    <FormInput type="pass1" label="Repeteix password:" input="inputPass1" :pass='pass' :reset='valid'></FormInput>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 </div>
--- src/scripts/forminput.js
+++ src/scripts/forminput.js
@@ -1,123 +1,134 @@
 export default {
-    name:'FormInput',
-    props:['type','input','label','pass'],
-    data(){
-      return{
-        inp:'',
-        error:true,
-        msg:'',
-        valid:'',
-        
-        }
-    },
-    watch:{
-      inp: function(){
-        this.error =false;
-        this.msg='';
-        if(this.$props.input == 'inputName'){
-          
-          if(!this.isRequired(this.inp)||!this.isText(this.inp)||!this.validLength(this.inp)){
-            this.error=true;
-            this.msg='El valor no pot ser buït, ha de ser text i ha de contenir entre 6 i 13 caràcters.';
-            this.isInvalid()
-          }else{
-              this.isValid()
-          }
-
-        }else if (this.$props.input == 'inputMobile'){
-          if(!this.isRequired(this.inp)||!this.isNumber(this.inp)){
-            this.error=true;
-            this.msg='El valor no pot ser buït, ha de ser un número';
-            this.isInvalid()
-          }else{
-              this.isValid()
-          }
-        }else if (this.$props.input == 'inputPostal'){
-          if(!this.isRequired(this.inp)||!this.isNumber(this.inp)){
-            this.error=true;
-            this.msg='El valor no pot ser buït, ha de ser un número';
-            this.isInvalid()
-          }else{
-              this.isValid()
-          }
-        }else if (this.$props.input == 'inputEmail'){
-          if(!this.isRequired(this.inp)||!this.isEmail(this.inp)){
-            this.error=true;
-            this.msg='El valor no pot ser buït, ha de ser un email vàlid';
-            this.isInvalid()
-          }else{
-              this.isValid()
-          }
-        }else if (this.$props.input == 'inputPass'){
-          if(!this.isRequired(this.inp)||!this.isUpperAndLower(this.inp)||!this.validLength(this.inp)){
-            this.error=true;
-            this.msg='El valor no pot ser buït, ha de contenir una minúscula i una majúscula i entre 6 i 13 caràcters,';
-            this.isInvalid()
-          }else{
-             this.$emit('pass',this.inp);
-              
-            this.isValid()
-          }
-        }else if (this.$props.input == 'inputPass1'){
-          if(this.inp != this.pass){
-            this.error=true;
-            this.msg='El password ha de coincidir';
-            this.isInvalid()
-          }else{
-              this.isValid()
-          }
-        }
-
+  name: 'FormInput',
+  props: ['type', 'input', 'label', 'pass', 'reset'],
+  data() {
+    return {
+      inp: '',
+      error: true,
+      msg: '',
+      valid: '',
+      skip: ''
+    }
+  },
+  watch: {
+    reset: function () {
+      if (this.reset == true) {
+        this.inp = '';
+        this.skip = true;
       }
     },
-    methods:{
-      isValid(){
-        this.valid = "is-valid"
-      },
+    inp: function () {
+      this.error = false;
+      this.msg = '';
+      if (this.$props.input == 'inputName' && !this.skip) {
 
-      isInvalid(){
-        this.valid = "is-invalid"
-
-      },
-
-      isRequired(value){
-        if(value.length===0){
-          console.log("empty")
-          return false
-        }else{
-          return true
-        }
-      },
-
-      isText(value){
-        
-        return /^[A-Za-z]+$/.test(value);
-      },
-
-      isNumber(value){
-        return /^[0-9]*$/.test(value);
-      },
-
-      isUpperAndLower(value){      
-        return /(?=.*[a-z])(?=.*[A-Z])/.test(value);
-      },
-
-      isEmail(value){
-        return /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
-      },
-
-      validLength(value){
-        console.log(value.length);
-        if(value.length >= 6 && value.length < 14) {
-          
-          return true;
-        }else{
-          console.log("not valid length");
-          return false;
+        if (!this.isRequired(this.inp) || !this.isText(this.inp) || !this.validLength(this.inp, 6, 14)) {
+          this.error = true;
+          this.msg = 'El valor no pot ser buït, ha de ser text i ha de contenir entre 6 i 13 caràcters.';
+          this.isInvalid()
+        } else {
+          this.isValid()
         }
 
-      },
-      
+      } else if (this.$props.input == 'inputMobile' && !this.skip) {
+        if (!this.isRequired(this.inp) || !this.isNumber(this.inp) || !this.validLength(this.inp, 9, 12)) {
+          this.error = true;
+          this.msg = 'El valor no pot ser buït, ha de ser un número entre 9 i 11 dígits';
+          this.isInvalid()
+        } else {
+          this.isValid()
+        }
+      } else if (this.$props.input == 'inputPostal' && !this.skip) {
+        if (!this.isRequired(this.inp) || !this.isNumber(this.inp) || !this.validLength(this.inp, 5, 6)) {
+          this.error = true;
+          this.msg = 'El valor no pot ser buït, ha de ser un número de 5 dígits';
+          this.isInvalid()
+        } else {
+          this.isValid()
+        }
+      } else if (this.$props.input == 'inputEmail' && !this.skip) {
+        if (!this.isRequired(this.inp) || !this.isEmail(this.inp)) {
+          this.error = true;
+          this.msg = 'El valor no pot ser buït, ha de ser un email vàlid';
+          this.isInvalid()
+        } else {
+          this.isValid()
+        }
+      } else if (this.$props.input == 'inputPass' && !this.skip) {
+        if (!this.isRequired(this.inp) || !this.isUpperAndLower(this.inp) || !this.validLength(this.inp, 6, 14)) {
+          this.error = true;
+          this.msg = 'El valor no pot ser buït, ha de contenir una minúscula i una majúscula i entre 6 i 13 caràcters,';
+          this.isInvalid()
+        } else {
+          this.$emit('pass', this.inp);
+
+          this.isValid()
+        }
+      } else if (this.$props.input == 'inputPass1' && !this.skip) {
+        if (this.inp != this.pass) {
+          this.error = true;
+          this.msg = 'El password ha de coincidir';
+          this.isInvalid()
+        } else {
+          this.isValid()
+        }
+      }
+      if (this.skip) {
+        this.skip = false;
+        this.valid = '';
+        this.error = true;
+      }
+
     }
-    
+  }
+    ,
+  methods: {
+    isValid() {
+      this.valid = "is-valid"
+    },
+
+    isInvalid() {
+      this.valid = "is-invalid"
+
+    },
+
+    isRequired(value) {
+      if (value.length === 0) {
+        return false
+      } else {
+        return true
+      }
+    },
+
+    isText(value) {
+
+      return /^[A-Za-z\s]+$/.test(value);
+    },
+
+    isNumber(value) {
+      return /^[0-9]*$/.test(value);
+    },
+
+    isUpperAndLower(value) {
+      return /(?=.*[a-z])(?=.*[A-Z])/.test(value);
+    },
+
+    isEmail(value) {
+      return /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
+    },
+
+    validLength(value, min, max) {
+      if (value.length >= min && value.length < max) {
+
+        return true;
+      } else {
+        console.log("not valid length");
+        return false;
+      }
+
+    },
+
+  },
+
+  
 }
